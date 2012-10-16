ca0afe942f9518cb6ae5d46a1ee94b3ef82ae26f
Oscar Esteban <oesteban@die.upm.es>
2012-10-16 10:23:49 -0700

    New optimizers structure

--- Code/src/Optimizers/include/GradientDescentEnergyOptimizer.h
+++ Code/src/Optimizers/include/GradientDescentEnergyOptimizer.h
@@ -40,6 +40,7 @@
 
 #include <itkObject.h>
 #include "EnergyOptimizer.h"
+#include <itkWindowConvergenceMonitoringFunction.h>
 #include <vector>
 
 namespace rstk
@@ -48,21 +49,79 @@ namespace rstk
 template< class TEnergy >
 class GradientDescentEnergyOptimizer: public EnergyOptimizer< TEnergy > {
 public:
+	/** Standard class typedefs and macros */
 	typedef GradientDescentEnergyOptimizer             Self;
-	typedef itk::Object                                Superclass;
+	typedef EnergyOptimizer< TEnergy >                 Superclass;
 	typedef SmartPointer<Self>                         Pointer;
 	typedef SmartPointer< const Self >                 ConstPointer;
-	typedef EnergyOptimizer< TEnergy >                 Superclass;
+	itkTypeMacro( GradientDescentEnergyOptimizer, EnergyOptimizer ); // Run-time type information (and related methods)
+	itkNewMacro( Self );                                             // New macro for creation of through a Smart Pointer
 
-	itkTypeMacro( GradientDescentEnergyOptimizer, EnergyOptimizer );
-	itkNewMacro( Self );
-
+	/** Energy-related typedefs */
 	typedef typename Superclass::EnergyType            EnergyType;
 	typedef typename EnergyType::Pointer               EnergyPointer;
 	typedef typename EnergyType::EnergyValueType       EnergyValueType;
 	typedef typename EnergyType::DisplacementFieldType DisplacementFieldType;
 	typedef typename DisplacementFieldType::Pointer    DisplacementFieldPointer;
 
+	/** Derivative type */
+	typedef Superclass::DerivativeType      DerivativeType;
+
+	/** Metric type over which this class is templated */
+	typedef Superclass::MeasureType                  MeasureType;
+	typedef Superclass::InternalComputationValueType InternalComputationValueType;
+
+	/** Type for the convergence checker */
+	typedef itk::Function::WindowConvergenceMonitoringFunction<double>	ConvergenceMonitoringType;
+
+	itkSetMacro(LearningRate, InternalComputationValueType);               // Set the learning rate
+	itkGetConstReferenceMacro(LearningRate, InternalComputationValueType); // Get the learning rate
+
+	/** Minimum convergence value for convergence checking.
+	 *  The convergence checker calculates convergence value by fitting to
+	 *  a window of the energy profile. When the convergence value reaches
+	 *  a small value, it would be treated as converged.
+	 *
+	 *  The default m_MinimumConvergenceValue is set to 1e-8 to pass all
+	 *  tests. It is suggested to use 1e-6 for less stringent convergence
+	 *  checking.
+	 */
+	itkSetMacro(MinimumConvergenceValue, InternalComputationValueType);
+
+	/** Window size for the convergence checker.
+	 *  The convergence checker calculates convergence value by fitting to
+	 *  a window of the energy (metric value) profile.
+	 *
+	 *  The default m_ConvergenceWindowSize is set to 50 to pass all
+	 *  tests. It is suggested to use 10 for less stringent convergence
+	 *  checking.
+	 */
+	itkSetMacro(ConvergenceWindowSize, SizeValueType);
+
+	/** Get current convergence value */
+	itkGetConstReferenceMacro( ConvergenceValue, InternalComputationValueType );
+
+	/** Flag. Set to have the optimizer track and return the best
+	 *  best metric value and corresponding best parameters that were
+	 *  calculated during the optimization. This captures the best
+	 *  solution when the optimizer oversteps or osciallates near the end
+	 *  of an optimization.
+	 *  Results are stored in m_CurrentMetricValue and in the assigned metric's
+	 *  parameters, retrievable via optimizer->GetCurrentPosition().
+	 *  This option requires additional memory to store the best
+	 *  parameters, which can be large when working with high-dimensional
+	 *  transforms such as DisplacementFieldTransform.
+	 */
+	itkSetMacro(ReturnBestParametersAndValue, bool);
+	itkGetConstReferenceMacro(ReturnBestParametersAndValue, bool);
+	itkBooleanMacro(ReturnBestParametersAndValue);
+
+	/** Start and run the optimization */
+	virtual void StartOptimization();
+
+	virtual void StopOptimization(void);
+
+	virtual void ResumeOptimization();
 
 protected:
 	GradientDescentEnergyOptimizer();
