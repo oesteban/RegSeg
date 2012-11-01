c55da264f3aaf1315fe6270844813ee12bec0911
Oscar Esteban <oesteban@die.upm.es>
2012-11-01 11:23:17 -0700

    MahalanobisLevelSets v.1.0 finished It seems to be working out. Possible issues: IDW Basis function is too costly. Check Multivariate Sparse to Dense interpolators.

--- Code/Modules/LevelSets/test/MahalanobisLevelSetsTest.cxx
+++ Code/Modules/LevelSets/test/MahalanobisLevelSetsTest.cxx
@@ -99,9 +99,10 @@ int main(int argc, char *argv[]) {
 
 	DeformationFieldType::Pointer df = DeformationFieldType::New();
 	DeformationFieldType::SizeType size = im->GetLargestPossibleRegion().GetSize();
-	for( size_t i=0;i<3;i++) size[i]=(unsigned int) (size[i] * 0.25);
+	double factor = 1/3.2;
+	for( size_t i=0;i<3;i++) size[i]=(unsigned int) (size[i] * factor);
 	DeformationFieldType::SpacingType spacing = im->GetSpacing();
-	spacing*=4;
+	spacing/=factor;
 	df->SetRegions( size );
 	df->SetSpacing( spacing );
 	df->SetDirection( im->GetDirection() );
@@ -117,33 +118,38 @@ int main(int argc, char *argv[]) {
 	ls->SetParameters(mean1,cov, false);
 	ls->GetLevelSetsMap(df);
 
-//	typedef itk::Image<float,4u> FieldType;
-//	FieldType::Pointer out = FieldType::New();
-//	FieldType::SizeType size;
-//	size[0] = df->GetLargestPossibleRegion().GetSize()[0];
-//	size[1] = df->GetLargestPossibleRegion().GetSize()[1];
-//	size[2] = df->GetLargestPossibleRegion().GetSize()[2];
-//	size[3] = 3;
-//	out->SetRegions( size );
-//	out->SetSpacing( 1.0 );
-//	out->Allocate();
-//	out->FillBuffer(0.0);
-//
-//	float* buffer = out->GetBufferPointer();
-//	DeformationFieldType::PixelType* vectBuffer = df->GetBufferPointer();
-//	size_t nPix = df->GetLargestPossibleRegion().GetNumberOfPixels();
-//
-//	for(size_t pix = 0; pix< nPix; pix++) {
-//		DeformationFieldType::PixelType val = (*vectBuffer)[pix];
-//		for( size_t i=0; i<3; i++) {
-//			unsigned int idx = (i*nPix)+pix;
-//			buffer[idx] = (float) val[i];
-//		}
-//	}
-//	itk::ImageFileWriter<FieldType>::Pointer w = itk::ImageFileWriter<FieldType>::New();
-//	w->SetInput( out );
-//	w->SetFileName( std::string( TEST_DATA_DIR ) + "speed.nii.gz" );
-//	w->Update();
+	typedef itk::Image<float,4u> FieldType;
+	FieldType::Pointer out = FieldType::New();
+	FieldType::SizeType outSize;
+	outSize[0] = df->GetLargestPossibleRegion().GetSize()[0];
+	outSize[1] = df->GetLargestPossibleRegion().GetSize()[1];
+	outSize[2] = df->GetLargestPossibleRegion().GetSize()[2];
+	outSize[3] = 3;
+	out->SetRegions( outSize );
+	FieldType::SpacingType outSpacing;
+	outSpacing[0] = df->GetSpacing()[0];
+	outSpacing[1] = df->GetSpacing()[1];
+	outSpacing[2] = df->GetSpacing()[2];
+	outSpacing[3] = 1.0;
+	out->SetSpacing( outSpacing );
+	out->Allocate();
+	out->FillBuffer(0.0);
+
+	float* buffer = out->GetBufferPointer();
+	DeformationFieldType::PixelType* vectBuffer = df->GetBufferPointer();
+	size_t nPix = df->GetLargestPossibleRegion().GetNumberOfPixels();
+
+	for(size_t pix = 0; pix< nPix; pix++) {
+		DeformationFieldType::PixelType val = (*vectBuffer)[pix];
+		for( size_t i=0; i<3; i++) {
+			unsigned int idx = (i*nPix)+pix;
+			buffer[idx] = (float) val[i];
+		}
+	}
+	itk::ImageFileWriter<FieldType>::Pointer w = itk::ImageFileWriter<FieldType>::New();
+	w->SetInput( out );
+	w->SetFileName( std::string( TEST_DATA_DIR ) + "speed.nii.gz" );
+	w->Update();
 }
 
 
