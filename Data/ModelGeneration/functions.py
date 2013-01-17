08eac967fcc06f361789bd61bb752790c8929aca
Oscar Esteban <code@oscaresteban.es>
2013-01-17 11:00:21 +0100

    Added python script for contours plotting
    I open a general file for python utilities. The script has some trouble
    with some slices with complex shapes where spurious lines are presented,
    but in general it performs arguably well for visualization needed in papers
    
    Close #50

--- Data/ModelGeneration/functions.py
+++ Data/ModelGeneration/functions.py
@@ -3,6 +3,9 @@ import numpy as np
 from tvtk.api import tvtk
 from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
+from pandas import *
+import nibabel as nib
+import matplotlib.cm as cm
 
 def GenerateSurface( data, fname, use_smooth=True, spacing=(1.0,1.0,1.0), origin=(0.0,0.0,0.0) ):
     grid = tvtk.ImageData(spacing=spacing, origin=origin, dimensions=data.shape)
@@ -26,29 +29,50 @@ def GenerateSurface( data, fname, use_smooth=True, spacing=(1.0,1.0,1.0), origin
     return contour
 
 
-def FilterMesh(surf, sl, axis=2 ):
-    points = np.array(surf.points, dtype=np.float32 )
-    filtered = points[(points[:,axis]>(sl-0.25)) & (points[:,axis]<(sl+0.25)) ]
-    filtered[:,axis] = sl
-    uniq = np.array(list(set(tuple(p) for p in filtered)))
-    mesh = tvtk.PolyData( points=uniq )
-    mesh.point_data.scalars = np.zeros( np.shape(uniq)[0], 'f' )
-    mesh.point_data.scalars.name = 'scalars'
-#    surf = tvtk.SurfaceReconstructionFilter(input=mesh)
-#    surf.update()
-#    cont = tvtk.ContourFilter( input=mesh )
-#    cont.update()
-#    rev = tvtk.ReverseSense( input=cont.output, reverse_cells=True, reverse_normals=True)
-#    rev.update()
-#    mesh = cont.output
-    return mesh
+def FilterMesh(points, sl, zdim, fname=None ):
+    lower = sl <= zdim*0.5
+#    filtered = np.around( points[(points[:,2]>(sl-0.25)) & (points[:,2]<(sl+0.25))  ] )
+    if lower:
+        filtered = np.around( points[(points[:,2]<(sl+0.25))])
+    else:
+        filtered = np.around( points[(points[:,2]>(sl-0.25))])
+
+#    filtered = points[(points[:,2]>(sl-0.25))]
+#    filtered = np.array(list(set(tuple(p) for p in filtered)))
+    filtered.view('f,f,f').sort(order=['f2'], axis=0)
+    uniq = np.array( DataFrame(filtered).drop_duplicates(cols=[0,1],take_last=lower).values )
+    if not fname==None:
+        mesh = tvtk.PolyData( points=uniq )
+        mesh.point_data.scalars = np.zeros( np.shape(uniq)[0], 'f' )
+        mesh.point_data.scalars.name = 'scalars'
+        w = tvtk.PolyDataWriter(input=mesh, file_name=fname )
+        w.write()
+    return uniq,lower
 
 def ComputeContourFunction( image, surf, sl, axis=2, fig=None, color='r' ):
-    dim = ( image.get_shape()[0],image.get_shape()[1],image.get_shape()[2] )
-    dim2d = tuple(np.delete( np.array(dim), axis ))
-    mesh = FilterMesh( surf, sl, axis )
-    ps = np.array( mesh.points, dtype=np.float32 )[:,0:2]
-    d = np.ones( ps.shape[0] ).astype( np.float32 )    
+    points = np.array(surf.points, dtype=np.float32 )
+    if axis==1:
+        points[:,[0,1,2]] = points[:,[0,2,1]]
+    elif axis==0:
+        points[:,[0,1,2]] = points[:,[2,1,0]]
+
+    dim = np.shape( np.swapaxes( image.get_data(), axis, 2 ) )
+    dim2d = ( dim[0], dim[1] )
     xi,yi = np.mgrid[0:dim2d[0],0:dim2d[1]] 
-    interpolated = griddata( ps, d, (xi,yi), method='linear', fill_value=0.0 )
-    return interpolated
+    ps,lower = FilterMesh( points, sl, dim[2], 'Model1/test_pandas.vtk' )
+    if lower:
+        z_level = sl - 0.05
+    else:
+        z_level = sl + 0.05
+    interpolated = griddata( ps[:,[0,1]], ps[:,2] , (xi,yi), method='cubic', fill_value=z_level )
+    return interpolated,z_level
+
+def PlotContour( ref_path, surfs, ax=2, sl=50, colors=[ 'b','r','g','y'] ):
+    image = nib.load( ref_path )
+    sliced = np.swapaxes( image.get_data(), ax, 2)[:,:,sl]
+    cs = [ plt.imshow( sliced, cmap = cm.Greys_r ) ]
+    
+    for i,s in enumerate(surfs):
+    	pts,z_level = ComputeContourFunction( image, s, sl, axis=ax )
+        cs.append( plt.contour( pts, linewidths=2.5, colors=colors[i], levels=[z_level] ) )
+    return cs
