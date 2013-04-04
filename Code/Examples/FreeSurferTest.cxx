bd2725346379e478f1f17055848c6be1371fa36e
Oscar Esteban <code@oscaresteban.es>
2013-04-04 15:44:48 +0200

    Added some data for testing #40
    Additionally, a bug has been found on vtk generation routines, open #60.

--- Code/Examples/FreeSurferTest.cxx
+++ Code/Examples/FreeSurferTest.cxx
@@ -36,5 +36,40 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
+/*
+ * Test 1 - Read vtk mesh, save it as FreeSurfer binary mesh and open it with tksurfer
+ * Test 2 - Read previous FreeSurfer mesh, launch registration and save result as FreeSurfer binary mesh.
+ */
+#ifndef DATA_DIR
+#define DATA_DIR "../Data/FreeSurferTest/"
+#endif
 
+#include <itkQuadEdgeMesh.h>
+#include <itkImage.h>
+#include <itkVector.h>
+#include <itkVTKPolyDataReader.h>
+#include <itkVTKPolyDataWriter.h>
+#include "MahalanobisLevelSets.h"
 
+using namespace rstk;
+
+int main(int argc, char *argv[]) {
+	typedef itk::Vector<float, 2u>                               VectorPixelType;
+	typedef itk::Image<VectorPixelType, 3u>                      ImageType;
+	typedef MahalanobisLevelSets<ImageType>                      LevelSetsType;
+	typedef LevelSetsType::ContourDeformationType                ContourDeformationType;
+	typedef ContourDeformationType::Pointer                      ContourDisplacementFieldPointer;
+	typedef itk::VTKPolyDataReader< ContourDeformationType >     ReaderType;
+	typedef itk::VTKPolyDataWriter< ContourDeformationType >     WriterType;
+
+	ReaderType::Pointer polyDataReader = ReaderType::New();
+	polyDataReader->SetFileName( std::string( DATA_DIR ) + "new_priors_wm.vtk" );
+	polyDataReader->Update();
+	ContourDisplacementFieldPointer initialContour = polyDataReader->GetOutput();
+
+	WriterType::Pointer polyDataWriter = WriterType::New();
+	polyDataWriter->SetInput( initialContour );
+	polyDataWriter->SetFileName( std::string( DATA_DIR ) + "new_priors_wm.fsb" );
+
+	polyDataWriter->Update();
+}
