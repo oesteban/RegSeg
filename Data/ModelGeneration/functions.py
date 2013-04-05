695f08ddc3e714c38d8ca3054288a59abf5eb3c2
Oscar Esteban <code@oscaresteban.es>
2013-04-05 13:18:54 +0200

    New functions for writing VTK and freesurfer surfs
    Now it is possible to write 'legacy' VTK (plain ascii surface definition)
    and freesurfer ascii meshes when the vertex and faces are well defined.
    
    I checked visually orientation and consistency with paraview for VTK and
    tkmedit for freesurfer ascii. There still remains a little offset with
    freesurfer, but I'm not sure that it'll happen with real images.
    
    For now, I close #60. Then re-open if new issues are found.

--- Data/ModelGeneration/functions.py
+++ Data/ModelGeneration/functions.py
@@ -7,6 +7,7 @@ from pandas import *
 import nibabel as nib
 import matplotlib.cm as cm
 import datetime as dt
+import os
 
 def GenerateSurface( data, fname, use_smooth=True, spacing=(1.0,1.0,1.0), origin=(0.0,0.0,0.0) ):
     grid = tvtk.ImageData(spacing=spacing, origin=origin, dimensions=data.shape)
@@ -25,24 +26,30 @@ def GenerateSurface( data, fname, use_smooth=True, spacing=(1.0,1.0,1.0), origin
         contour = vtkmeshclean2.output
     contour.point_data.scalars = np.ones( np.shape( contour.points )[0] )
     contour.point_data.scalars.name = 'scalars'
-    vtk2fsa( contour, '%s.asc' % os.path.splitext( fname )[0] )
-    w = tvtk.PolyDataWriter(input=contour, file_name=fname )
-    w.write()
+    new_origin = tuple( (np.array(data.shape).astype(float)) * -0.5 ) # fix new origin to have a nice view in tkmedit 
+    vtk2fsa( contour, '%s.asc' % os.path.splitext( fname )[0], origin=new_origin)
+    #w = tvtk.PolyDataWriter(input=contour, file_name=fname )
+    #w.write()
+    savevtk( contour, fname )
     return contour
 
-def vtk2fsa( vtk, fname ):
+def vtk2fsa( vtk, fname, origin=(0.0,0.0,0.0) ):
     ncells = vtk.polys.number_of_cells
     npoints = len( vtk.points )
     nvalues = len( vtk.polys.data )
     cells = np.reshape( vtk.polys.data, ( ncells, int(nvalues/ncells) ) )
     now = dt.datetime.now()
+    points = vtk.points + np.array(origin)
+
+    if os.path.exists( fname ):
+        os.remove( fname )
 
     try:
         with open(fname, 'a+') as f:
             f.write( '#!ascii freesurfer mesh generated from vtk - %s, generated on %s\n' % (os.path.basename(fname),now.strftime("%Y-%m-%d %H:%M"))) # Write header
             f.write( '%d %d\n' % ( npoints, ncells ) ) # Write number of vertices and number of faces
             
-            for row in vtk.points:
+            for row in points:
                 f.write( '%.6f %.6f %.6f 0\n' % tuple(row) )
     
             for row in cells:
@@ -52,6 +59,37 @@ def vtk2fsa( vtk, fname ):
         return 0
 
     return 1
+
+def savevtk( vtk, fname, origin=(0.0,0.0,0.0) ):
+    ncells = vtk.polys.number_of_cells
+    npoints = len( vtk.points )
+    nvalues = len( vtk.polys.data )
+    cells = np.reshape( vtk.polys.data, ( ncells, int(nvalues/ncells) ) )
+    now = dt.datetime.now()
+    points = vtk.points + np.array(origin)
+
+    if os.path.exists( fname ):
+        os.remove( fname )
+
+    try:
+        with open(fname, 'a+') as f:
+            f.write( '# vtk DataFile Version 2.0\n' ) # Write identification
+            f.write( '%s, generated on %s\n' % (os.path.basename(fname),now.strftime("%Y-%m-%d %H:%M"))) # Write comment (up to 256 chars)
+            f.write( 'ASCII\n' )
+            f.write( 'DATASET POLYDATA\n' )
+            f.write( 'POINTS %d float\n' % npoints ) 
+            for row in points:
+                f.write( '%.6f %.6f %.6f\n' % tuple(row) )
+    
+            f.write( 'POLYGONS %d %d\n' % ( ncells, nvalues ) )
+            for row in cells:
+                f.write( '3 %d %d %d\n' % tuple( row[1:] ) )
+    except Exception as err:
+        print err
+        return 0
+
+    return 1
+
 
 
 
