1113d54e8be3a67221e345daf1834cb6be16fbf1
Oscar Esteban <code@oscaresteban.es>
2013-10-03 19:58:42 +0200

    Last version for 1st deadline ISBI 2014

--- Scripts/workflows/evaluation.py
+++ Scripts/workflows/evaluation.py
@@ -5,6 +5,7 @@
 
 import os.path as op
 import os
+import numpy as np
 
 import nipype.pipeline.engine as pe
 import dmri as dmri
@@ -17,7 +18,14 @@ import nipype.interfaces.ants as ants
 import nipype.workflows.dmri.fsl as wf
 
 
-def evaluation_workflow( name="Evaluation" ):
+def evaluation_workflow( name="Evaluation",
+                         te_incr=2.46e-3,
+                         dwell_time=0.77e-3,
+                         intensity=5.0e-2,
+                         sigma=6.0,
+                         direction='y' ):
+
+
     evaluation = pe.Workflow( name=name )
     
     inputnode = pe.Node( niu.IdentityInterface( fields=['in_dwi','in_dwi_mask',
@@ -29,28 +37,23 @@ def evaluation_workflow( name="Evaluation" ):
    
     outputnode = pe.Node( niu.IdentityInterface( fields=['out_tpms_resampled'] ),
                           name='outputnode' )
- 
-    te_incr = 2.46e-3 # secs
-    dwell_time = 0.77e-3 # secs
-    intensity = 0.75
-    sigma = 8.0
     
-    dist1_wf = dmri.distortion_workflow( name='Distortion_Y' )
+    dist1_wf = dmri.distortion_workflow( name='Distortion' )
     dist1_wf.inputs.inputnode.te_incr = te_incr
     dist1_wf.inputs.inputnode.echospacing = dwell_time
-    dist1_wf.inputs.inputnode.encoding_direction = 'y'
+    dist1_wf.inputs.inputnode.encoding_direction = direction
     dist1_wf.inputs.inputnode.intensity = intensity
     dist1_wf.inputs.inputnode.sigma = sigma
 
 
     resample= pe.MapNode( fs.MRIConvert(out_type='niigz'), iterfield=['in_file'], name='Resample')
-    normalize_tpms = pe.Node( niu.Function( input_names=['in_files'], output_names=['out_files'], function=dmri.normalize ), name='Normalize' )
+    norm_tpms = pe.Node( niu.Function( input_names=['in_files'], output_names=['out_files'], function=dmri.normalize ), name='Normalize' )
 
-    sel1 = pe.Node( niu.Select(index=3 ), name='SelectWM' )
-    sel2 = pe.Node( niu.Select(index=3 ), name='SelectWM_dist' )
-    sel3 = pe.Node( niu.Select(index=3 ), name='SelectWM_fmap' )
-    sel4 = pe.Node( niu.Select(index=3 ), name='SelectWM_topup' )
-    sel5 = pe.Node( niu.Select(index=3 ), name='SelectWM_t2' )
+    sel1 = pe.Node( niu.Select(index=[-1] ), name='SelectWM' )
+    sel2 = pe.Node( niu.Select(index=[-1] ), name='SelectWM_dist' )
+    sel3 = pe.Node( niu.Select(index=[-1] ), name='SelectWM_fmap' )
+    sel4 = pe.Node( niu.Select(index=[-1] ), name='SelectWM_topup' )
+    sel5 = pe.Node( niu.Select(index=[-1] ), name='SelectWM_t2' )
 
     sim1 = compare_dwis( name="Sim_dist" )
     sim1.inputs.inputnode.name='dist' 
@@ -66,10 +69,10 @@ def evaluation_workflow( name="Evaluation" ):
     ove3 = overlap_tpms_t2reg()
 
 
-    dist2_wf = dmri.distortion_workflow( name='Distortion_Yrev' )
+    dist2_wf = dmri.distortion_workflow( name='Distortion_rev' )
     dist2_wf.inputs.inputnode.te_incr = te_incr
     dist2_wf.inputs.inputnode.echospacing = dwell_time
-    dist2_wf.inputs.inputnode.encoding_direction = 'y-'
+    dist2_wf.inputs.inputnode.encoding_direction = direction + '-'
     dist2_wf.inputs.inputnode.intensity = intensity
     dist2_wf.inputs.inputnode.sigma = sigma
             
@@ -77,14 +80,15 @@ def evaluation_workflow( name="Evaluation" ):
     fmap_wf.inputs.inputnode.te_diff = te_incr*1e3 # in ms.
     fmap_wf.inputs.inputnode.epi_echospacing = dwell_time*1e3 # in ms
     fmap_wf.inputs.inputnode.vsm_sigma = 2.0
-    fmap_wf.inputs.inputnode.encoding_direction = 'y'
+    fmap_wf.inputs.inputnode.encoding_direction = direction
     
     t2reg_wf = dmri.t2_registration_correct()
     
     topup_wf = wf.topup_correction()
-    topup_wf.inputs.inputnode.encoding_direction = 'y'
     topup_wf.inputs.inputnode.ref_num = 0
-    
+    topup_wf.inputs.inputnode.readout_times = [ dwell_time*1e3, dwell_time*1e3 ]
+ 
+  
     trk_gold = dmri.dtk_tractography_workflow(name='Tract_Gold')
     trk_fmap = trk_gold.clone('Tract_Fmap' )
     trk_t2re = trk_gold.clone('Tract_T2Reg' )
@@ -94,14 +98,14 @@ def evaluation_workflow( name="Evaluation" ):
     evaluation.connect([
         # Prepare tpms
          (inputnode,       resample, [ ('in_tpms','in_file'),(('in_dwi',dmri.get_vox_size),'vox_size') ])
-        ,(resample,  normalize_tpms, [ ('out_file','in_files')])
-        ,(normalize_tpms,outputnode, [ ('out_files','out_tpms_resampled')])
-        ,(normalize_tpms,      sel1, [ ('out_files','inlist')])
+        ,(resample,  norm_tpms, [ ('out_file','in_files')])
+        ,(norm_tpms,outputnode, [ ('out_files','out_tpms_resampled')])
+        ,(norm_tpms,      sel1, [ ('out_files','inlist')])
         # Connect the distortion workflows
         ,(inputnode,      dist1_wf, [ ('in_dwi','inputnode.in_file'),('in_dwi_mask','inputnode.in_mask') ])
-        ,(normalize_tpms, dist1_wf, [ ('out_files','inputnode.in_tpms') ])
+        ,(norm_tpms, dist1_wf, [ ('out_files','inputnode.in_tpms') ])
         ,(inputnode,      dist2_wf, [ ('in_dwi','inputnode.in_file'),('in_dwi_mask','inputnode.in_mask') ])
-        ,(normalize_tpms, dist2_wf, [ ('out_files','inputnode.in_tpms') ])
+        ,(norm_tpms, dist2_wf, [ ('out_files','inputnode.in_tpms') ])
         ,(dist1_wf,           sel2, [ ('outputnode.out_tpms','inlist')])
         # Similarity after distortion
         ,(dist1_wf,           sim1, [ ('outputnode.out_file','inputnode.in_test') ])
@@ -112,12 +116,6 @@ def evaluation_workflow( name="Evaluation" ):
         # Similarity after fieldmap
         ,(fmap_wf,            sim2, [ ('outputnode.epi_corrected','inputnode.in_test') ])
         ,(inputnode,          sim2, [ ('in_dwi', 'inputnode.in_ref'),('in_dwi_mask','inputnode.in_mask'),('out_csv','inputnode.out_csv') ])
-        # Correct with topup
-        ,(dist1_wf,       topup_wf, [ ('outputnode.out_file','inputnode.in_file_dir') ])
-        ,(dist2_wf,       topup_wf, [ ('outputnode.out_file','inputnode.in_file_rev') ])
-        # Similarity after topup
-        ,(topup_wf,           sim3, [ ('outputnode.epi_corrected','inputnode.in_test') ])
-        ,(inputnode,          sim3, [ ('in_dwi', 'inputnode.in_ref'),('in_dwi_mask','inputnode.in_mask'),('out_csv','inputnode.out_csv') ])
         # Correct with registration
         ,(dist1_wf,       t2reg_wf, [ ('outputnode.out_file','inputnode.in_file'),('outputnode.out_mask','inputnode.in_mask_dwi') ])
         ,(inputnode,      t2reg_wf, [ ('in_t2','inputnode.in_t2'),('in_t2_mask','inputnode.in_mask_t2')])
@@ -128,18 +126,12 @@ def evaluation_workflow( name="Evaluation" ):
         # Overlaps
         #
         ,(inputnode,          ove1, [ ('out_csv','inputnode.out_csv') ])
-        ,(normalize_tpms,     ove1, [ ('out_files','inputnode.in_ref') ])
+        ,(norm_tpms,     ove1, [ ('out_files','inputnode.in_ref') ])
         ,(fmap_wf,            ove1, [ ('outputnode.out_vsm','inputnode.in_vsm') ])
         ,(dist1_wf,           ove1, [ ('outputnode.out_tpms','inputnode.in_tpms' ) ])
         ,(ove1,               sel3, [ ('outputnode.out_tpms','inlist')])
-        ,(inputnode,          ove2, [ ('out_csv','inputnode.out_csv') ])
-        ,(normalize_tpms,     ove2, [ ('out_files','inputnode.in_ref') ])
-        ,(topup_wf,           ove2, [ ('outputnode.out_topup','inputnode.in_topup'),('outputnode.out_enc_file','inputnode.in_enc_file') ])
-        ,(dist1_wf,           ove2, [ ('outputnode.out_tpms','inputnode.in_tpms' ) ])
-        ,(dist2_wf,           ove2, [ ('outputnode.out_tpms','inputnode.in_tpms_rev') ])
-        ,(ove2,               sel4, [ ('outputnode.out_tpms','inlist')])
         ,(inputnode,          ove3, [ ('out_csv','inputnode.out_csv') ])
-        ,(normalize_tpms,     ove3, [ ('out_files','inputnode.in_ref') ])
+        ,(norm_tpms,     ove3, [ ('out_files','inputnode.in_ref') ])
         ,(t2reg_wf,           ove3, [ ('outputnode.fwd_tfm','inputnode.in_tfm') ])
         ,(dist1_wf,           ove3, [ ('outputnode.out_tpms','inputnode.in_tpms' ) ])
         ,(ove3,               sel5, [ ('outputnode.out_tpms','inlist')])
@@ -160,17 +152,76 @@ def evaluation_workflow( name="Evaluation" ):
                                       ('in_bvec','inputnode.in_bvec'),('in_bval','inputnode.in_bval' ) ])
         ,(fmap_wf,        trk_fmap, [ ('outputnode.epi_corrected','inputnode.in_dwi' ) ])
         ,(sel3,           trk_fmap, [ ('out','inputnode.in_mask') ])
-        # Perform tractography on the topup corrected
-        ,(inputnode,       trk_rev, [ ('in_rois','inputnode.roi_file'),
-                                      ('in_bvec','inputnode.in_bvec'),('in_bval','inputnode.in_bval' ) ])
-        ,(topup_wf,        trk_rev, [ ('outputnode.epi_corrected','inputnode.in_dwi' ) ])
-        ,(sel4,            trk_rev, [ ('out','inputnode.in_mask') ])
         # Perform tractography on the t2-registration corrected
         ,(inputnode,      trk_t2re, [ ('in_rois','inputnode.roi_file'),
                                       ('in_bvec','inputnode.in_bvec'),('in_bval','inputnode.in_bval' ) ])
         ,(t2reg_wf,       trk_t2re, [ ('outputnode.epi_corrected','inputnode.in_dwi' ) ])
         ,(sel5,           trk_t2re, [ ('out','inputnode.in_mask') ])
     ])
+
+
+    if direction == 'z':
+        topup_wf.inputs.inputnode.encoding_direction = 'y'
+        swap_dir = pe.Node( niu.Function( input_names=['in_file'],output_names=['out_file'],function=swap_yz),name='SwapYZ_dir' )
+        swap_rev = pe.Node( niu.Function( input_names=['in_file'],output_names=['out_file'],function=swap_yz),name='SwapYZ_rev' )
+        swap_cor = pe.Node( niu.Function( input_names=['in_file'],output_names=['out_file'],function=swap_yz),name='SwapYZ_cor' )
+
+        sw_tpm_in = pe.MapNode( niu.Function( input_names=['in_file'],output_names=['out_file'],function=swap_yz), iterfield=['in_file'], name='SwapYZ_tpms_in')
+        sw_tpm_di = pe.MapNode( niu.Function( input_names=['in_file'],output_names=['out_file'],function=swap_yz), iterfield=['in_file'], name='SwapYZ_tpms_di')
+        sw_tpm_re = pe.MapNode( niu.Function( input_names=['in_file'],output_names=['out_file'],function=swap_yz), iterfield=['in_file'], name='SwapYZ_tpms_re')
+        sw_tpm_ou = pe.MapNode( niu.Function( input_names=['in_file'],output_names=['out_file'],function=swap_yz), iterfield=['in_file'], name='SwapYZ_tpms_ou')
+
+        evaluation.connect([
+            # Correct with topup
+             (dist1_wf,       swap_dir, [ ('outputnode.out_file','in_file') ])
+            ,(dist2_wf,       swap_rev, [ ('outputnode.out_file','in_file') ])
+            ,(swap_dir,       topup_wf, [ ('out_file','inputnode.in_file_dir') ])
+            ,(swap_rev,       topup_wf, [ ('out_file','inputnode.in_file_rev') ])
+            # Overlap
+            ,(inputnode,          ove2, [ ('out_csv','inputnode.out_csv') ])
+            ,(norm_tpms,     sw_tpm_in, [ ('out_files','in_file') ])
+            ,(sw_tpm_in,          ove2, [ ('out_file','inputnode.in_ref') ])
+            ,(topup_wf,           ove2, [ ('outputnode.out_topup','inputnode.in_topup'),('outputnode.out_enc_file','inputnode.in_enc_file') ])
+            ,(dist1_wf,      sw_tpm_di, [ ('outputnode.out_tpms','in_file' ) ])
+            ,(sw_tpm_di,          ove2, [ ('out_file','inputnode.in_tpms' ) ])
+            ,(dist2_wf,      sw_tpm_re, [ ('outputnode.out_tpms','in_file') ])
+            ,(sw_tpm_re,          ove2, [ ('out_file', 'inputnode.in_tpms_rev') ])
+            ,(ove2,          sw_tpm_ou, [ ('outputnode.out_tpms','in_file')])
+            ,(sw_tpm_ou,          sel4, [ ('out_file','inlist')])
+            # Similarity after topup
+            ,(topup_wf,       swap_cor, [ ('outputnode.epi_corrected','in_file') ])
+            ,(swap_cor,           sim3, [ ('out_file','inputnode.in_test') ])
+            ,(inputnode,          sim3, [ ('in_dwi', 'inputnode.in_ref'),('in_dwi_mask','inputnode.in_mask'),('out_csv','inputnode.out_csv') ])
+            # Perform tractography on the topup corrected
+            ,(inputnode,       trk_rev, [ ('in_rois','inputnode.roi_file'),
+                                          ('in_bvec','inputnode.in_bvec'),('in_bval','inputnode.in_bval' ) ])
+            ,(swap_cor,        trk_rev, [ ('out_file','inputnode.in_dwi' ) ])
+            ,(sel4,            trk_rev, [ ('out','inputnode.in_mask') ])
+        ])
+    else:
+        topup_wf.inputs.inputnode.encoding_direction = direction
+        evaluation.connect([
+            # Correct with topup
+             (dist1_wf,       topup_wf, [ ('outputnode.out_file','inputnode.in_file_dir') ])
+            ,(dist2_wf,       topup_wf, [ ('outputnode.out_file','inputnode.in_file_rev') ])
+            # Overlap
+            ,(inputnode,          ove2, [ ('out_csv','inputnode.out_csv') ])
+            ,(norm_tpms,          ove2, [ ('out_files','inputnode.in_ref') ])
+            ,(topup_wf,           ove2, [ ('outputnode.out_topup','inputnode.in_topup'),('outputnode.out_enc_file','inputnode.in_enc_file') ])
+            ,(dist1_wf,           ove2, [ ('outputnode.out_tpms','inputnode.in_tpms' ) ])
+            ,(dist2_wf,           ove2, [ ('outputnode.out_tpms','inputnode.in_tpms_rev') ])
+            ,(ove2,               sel4, [ ('outputnode.out_tpms','inlist')])
+            # Similarity after topup
+            ,(topup_wf,           sim3, [ ('outputnode.epi_corrected','inputnode.in_test') ])
+            ,(inputnode,          sim3, [ ('in_dwi', 'inputnode.in_ref'),('in_dwi_mask','inputnode.in_mask'),('out_csv','inputnode.out_csv') ])
+            # Perform tractography on the topup corrected
+            ,(inputnode,       trk_rev, [ ('in_rois','inputnode.roi_file'),
+                                          ('in_bvec','inputnode.in_bvec'),('in_bval','inputnode.in_bval' ) ])
+            ,(topup_wf,        trk_rev, [ ('outputnode.epi_corrected','inputnode.in_dwi' ) ])
+            ,(sel4,            trk_rev, [ ('out','inputnode.in_mask') ])
+        ])
+    
+
     return evaluation
 
 
@@ -301,7 +352,26 @@ def _append_csv( fname, row, rowname, feature ):
         w = csv.writer( f )
         w.writerow( row )
 
+def swap_yz( in_file, out_file=None ):
+    import nibabel as nib
+    import numpy as np
+    import os.path as op
+
+    if out_file is None:
+        fname, fext = op.splitext( op.basename( in_file ) )
+        if fext=='.gz':
+            fname, fext2 = op.splitext( fname )
+            fext = fext2+fext
+        out_file = op.abspath( fname + '_swapyz' + fext )
+
+    im = nib.load( in_file )
+    im_data = np.swapaxes( im.get_data(), 1, 2 )
+    nib.save( nib.Nifti1Image( im_data, im.get_affine(), im.get_header() ), out_file )
+    return out_file
+    
+
 if __name__ == '__main__':
+    z = False
     root_dir='/home/oesteban/workspace/ACWE-Reg/'
     data_dir= op.join( root_dir, 'Data' )
     mname = 'DigitalPhantomISBI'
@@ -316,7 +386,7 @@ if __name__ == '__main__':
         os.makedirs( working_dir )
 
     
-    evaluation = evaluation_workflow()
+    evaluation = evaluation_workflow( direction='z' )
     evaluation.base_dir = working_dir
 
 
@@ -324,17 +394,43 @@ if __name__ == '__main__':
     if op.exists( sim_file ):
         os.remove( sim_file )
 
-    evaluation.inputs.inputnode.in_dwi = op.join( model_dir, 'DWIS_dti-scheme_no-noise.nii.gz' )
-    evaluation.inputs.inputnode.in_dwi_mask = op.join( model_dir, 'signal_mask.nii.gz' )
-    evaluation.inputs.inputnode.in_t2 = op.join( model_dir, "t2_weighted.nii.gz" )
-    evaluation.inputs.inputnode.in_t2_mask = op.join( model_dir, "tpms", "tpm_mask.nii.gz" )
-    evaluation.inputs.inputnode.in_bvec = op.join( model_dir, 'dti-scheme.bvec' ) 
     evaluation.inputs.inputnode.in_bval = op.join( model_dir, 'dti-scheme.bval' )
-    evaluation.inputs.inputnode.in_rois = op.join( model_dir,'rois', 'seeding_regions.nii.gz' )
-    evaluation.inputs.inputnode.wm_mask = op.join( model_dir, 'wm_mask.nii.gz' )
-    evaluation.inputs.inputnode.in_tpms = [ op.join( model_dir, 'tpms', '%s_volume_fraction.nii.gz' % n ) for n in [ 'bg','csf','gm','wm' ] ]
     evaluation.inputs.inputnode.out_csv = sim_file
-    evaluation.run()
-    #evaluation.write_graph()
+    
+    in_dwi = op.join( model_dir, 'DWIS_dti-scheme_no-noise.nii.gz' )
+    in_dwi_mask = op.join( model_dir, 'rois', 'signal_mask.nii.gz' )
+    in_t2 = op.join( model_dir, "t2_weighted.nii.gz" )
+    in_t2_mask = op.join( model_dir, "tpms", "tpm_mask.nii.gz" )
+    in_rois = op.join( model_dir,'rois', 'seeding_regions.nii.gz' )
+    wm_mask = op.join( model_dir,'rois', 'fibers.nii.gz' )
+    in_tpms = [ op.join( model_dir, 'tpms', '%s_volume_fraction.nii.gz' % n ) for n in [ 'csf','gm','wm' ] ]
+    in_bvec = op.join( model_dir, 'dti-scheme.bvec' ) 
+    
+    if z == True:
+        inputs_dir = op.join( working_dir, 'inputs' )
+        if not op.exists( inputs_dir ):
+            os.makedirs( inputs_dir )
+        in_dwi = swap_yz( in_dwi, op.join( inputs_dir, op.basename( in_dwi ) ) )
+        in_dwi_mask = swap_yz( in_dwi_mask, op.join( inputs_dir, op.basename( in_dwi_mask ) ) ) 
+        in_t2 = swap_yz( in_t2, op.join( inputs_dir, op.basename( in_t2 ) ) ) 
+        in_t2_mask = swap_yz( in_t2_mask, op.join( inputs_dir, op.basename( in_t2_mask ) ) ) 
+        in_rois = swap_yz( in_rois, op.join( inputs_dir, op.basename( in_rois ) ) ) 
+        wm_mask = swap_yz( wm_mask, op.join( inputs_dir, op.basename( wm_mask ) ) ) 
 
+        for i,tpm in enumerate( in_tpms ):
+            in_tpms[i] = swap_yz( tpm, op.join( inputs_dir, op.basename( tpm ) ) )
+
+        bvec = np.loadtxt( in_bvec )
+        bvec[[1, 2],:] = bvec[[2,1],:]
+        np.savetxt( op.join( inputs_dir, op.basename(in_bvec)), bvec, fmt='%.5f' )
+ 
+    evaluation.inputs.inputnode.in_dwi = in_dwi
+    evaluation.inputs.inputnode.in_dwi_mask = in_dwi_mask
+    evaluation.inputs.inputnode.in_t2 = in_t2
+    evaluation.inputs.inputnode.in_t2_mask = in_t2_mask
+    evaluation.inputs.inputnode.in_rois = in_rois
+    evaluation.inputs.inputnode.wm_mask = wm_mask
+    evaluation.inputs.inputnode.in_tpms = in_tpms
+    evaluation.inputs.inputnode.in_bvec = in_bvec
+    evaluation.run()
 
