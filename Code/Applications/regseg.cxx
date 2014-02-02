525244e795aef34f1ce0176bb768cde963f55d29
Oscar Esteban <code@oscaresteban.es>
2014-02-03 00:58:54 +0100

    New interface #115
    Now it can parse configurations for several levels.
    Still, I have to check how to allow for multiple occurrences
    in boost program_options.

--- Code/Applications/regseg.cxx
+++ Code/Applications/regseg.cxx
@@ -36,39 +36,94 @@ int main(int argc, char *argv[]) {
 	bool outImages = false;
 	size_t verbosity = 1;
 
-	bpo::options_description desc("Usage");
-	desc.add_options()
+	bpo::options_description all_desc("Usage");
+	bpo::options_description general_desc("General options");
+	general_desc.add_options()
 			("help,h", "show help message")
 			("fixed-images,F", bpo::value < std::vector<std::string>	> (&fixedImageNames)->multitoken()->required(), "fixed image file")
 			("moving-surfaces,M", bpo::value < std::vector<std::string>	> (&movingSurfaceNames)->multitoken()->required(),	"moving image file")
 			("transform-levels,L", bpo::value< size_t > (), "number of multi-resolution levels for the transform")
 			("output-prefix,o", bpo::value < std::string > (&outPrefix), "prefix for output files")
 			("output-all", bpo::bool_switch(&outImages),"output intermediate images")
+			("logfile,l", bpo::value<std::string>(&logFileName), "log filename")
+			("verbosity,V", bpo::value<size_t>(&verbosity), "verbosity level ( 0 = no output; 5 = verbose )");
+
+	bpo::options_description level_desc("Registration level options");
+	level_desc.add_options()
 			("alpha,a", bpo::value< float > (), "alpha value in regularization")
 			("beta,b", bpo::value< float > (), "beta value in regularization")
 			("step-size,s", bpo::value< float > (), "step-size value in optimization")
 			("iterations,i", bpo::value< size_t > (), "number of iterations")
 			("grid-size,g", bpo::value< size_t > (), "grid size")
-			("descriptors-update-iterations,u", bpo::value< size_t > (), "frequency (iterations) to update descriptors of regions (0=no update)")
-			("logfile,l", bpo::value<std::string>(&logFileName), "log filename")
-			("verbosity,V", bpo::value<size_t>(&verbosity), "verbosity level ( 0 = no output; 5 = verbose )");
-	bpo::positional_options_description pdesc;
-	bpo::variables_map vmap;
-	bpo::store(
-			bpo::command_line_parser(argc, argv).options(desc).positional(pdesc).run(),
-			vmap);
+			("update-descriptors,u", bpo::value< size_t > (), "frequency (iterations) to update descriptors of regions (0=no update)");
+	//bpo::positional_options_description pdesc;
 
-	if (vmap.count("help")) {
-		std::cout << desc << std::endl;
+
+
+	std::vector< std::string > cli_token;
+	std::vector< std::string > cli_general;
+	std::vector< std::vector< std::string > > cli_levels;
+
+	bool isLevel = false;
+	std::string token;
+	for( int i = 0; i < argc; i++) {
+		token = argv[i];
+		if( !isLevel ) {
+			if ( token.at(0)=='[' ) {
+				isLevel = true;
+				token.erase(0,1);
+			} else {
+				cli_general.push_back( argv[i] );
+			}
+		}
+
+		if( isLevel ) {
+			if ( *token.rbegin() == ']' ) {
+				token = token.substr( 0, token.size() -1 );
+				isLevel = false;
+			}
+			cli_token.push_back( token );
+
+			if ( !isLevel ) {
+				cli_levels.push_back( cli_token );
+				cli_token.empty();
+			}
+		}
+	}
+
+	size_t cli_nlevels = cli_levels.size();
+
+	bpo::variables_map vm_general;
+	std::vector< bpo::variables_map > vm_levels;
+
+
+	all_desc.add( general_desc ).add( level_desc );
+	if( cli_nlevels == 0 ) {
+		bpo::store(	bpo::command_line_parser( cli_general ).options(all_desc).run(),vm_general);
+	} else {
+		bpo::store( bpo::command_line_parser( cli_general ).options(general_desc).run(), vm_general );
+
+		for ( size_t i = 0; i<cli_nlevels; i++ ) {
+			bpo::options_description ndesc;
+			ndesc.add( level_desc );
+			bpo::variables_map vm;
+			bpo::store(	bpo::command_line_parser( cli_levels[i] ).options(ndesc).run(),vm );
+
+			vm_levels.push_back( vm );
+		}
+	}
+
+	if (vm_general.count("help")) {
+		std::cout << all_desc << std::endl;
 		return 1;
 	}
 
 	try {
-		bpo::notify(vmap);
+		bpo::notify(vm_general);
 	} catch (boost::exception_detail::clone_impl
 			< boost::exception_detail::error_info_injector<	boost::program_options::required_option> > &err) {
 		std::cout << "Error: " << err.what() << std::endl;
-		std::cout << desc << std::endl;
+		std::cout << all_desc << std::endl;
 		return EXIT_FAILURE;
 	}
 
@@ -116,33 +171,33 @@ int main(int argc, char *argv[]) {
 	root["inputs"]["moving"]["components"] = movingjson;
 
 	// Set up registration ------------------------------------------------------------
-	if (vmap.count("transform-levels")) {
-		acwereg->SetNumberOfLevels( vmap["transform-levels"].as<size_t>() );
+	if (vm_general.count("transform-levels")) {
+		acwereg->SetNumberOfLevels( vm_general["transform-levels"].as<size_t>() );
 		acwereg->SetUseGridLevelsInitialization( true );
 	}
-	if (vmap.count("iterations")) {
-		acwereg->FillNumberOfIterations( vmap["iterations"].as< size_t >() );
+	if (vm_general.count("iterations")) {
+		acwereg->FillNumberOfIterations( vm_general["iterations"].as< size_t >() );
 	}
 
-	if (vmap.count("step-size")) {
-		acwereg->FillStepSize( vmap["step-size"].as<float>() );
+	if (vm_general.count("step-size")) {
+		acwereg->FillStepSize( vm_general["step-size"].as<float>() );
 
 	}
-	if (vmap.count("alpha")) {
-		acwereg->FillAlpha( vmap["alpha"].as<float>() );
+	if (vm_general.count("alpha")) {
+		acwereg->FillAlpha( vm_general["alpha"].as<float>() );
 	}
 
-	if (vmap.count("beta")) {
-		acwereg->FillBeta( vmap["beta"].as<float>() );
+	if (vm_general.count("beta")) {
+		acwereg->FillBeta( vm_general["beta"].as<float>() );
 	}
-	if (vmap.count("descriptors-update-iterations")) {
-		size_t updDesc =  vmap["descriptors-update-iterations"].as<size_t>();
+	if (vm_general.count("descriptors-update-iterations")) {
+		size_t updDesc =  vm_general["descriptors-update-iterations"].as<size_t>();
 		acwereg->FillDescriptorRecomputationFreq(updDesc);
 	}
 
 
-	//if (vmap.count("grid-size")) {
-	//	acwereg->SetGridSize( vmap["grid-size"].as<size_t>() );
+	//if (vm_general.count("grid-size")) {
+	//	acwereg->SetGridSize( vm_general["grid-size"].as<size_t>() );
 	//}
     //
 
