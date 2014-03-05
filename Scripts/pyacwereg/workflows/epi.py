b34a116880a51338feffaaaa758eb34904fad16a
oesteban <code@oscaresteban.es>
2014-03-05 12:50:02 +0100

    Added ISBI workflow adaptation
    Close #117

--- Scripts/pyacwereg/workflows/epi.py
+++ Scripts/pyacwereg/workflows/epi.py
@@ -14,6 +14,32 @@ import nipype.interfaces.ants as ants           # ANTS
 import nipype.pipeline.engine as pe
 
 
+def isbi_workflow( name='ISBI2014' ):
+    workflow = pe.Workflow(name=name)
+
+    # Setup i/o
+    inputnode = pe.Node( niu.IdentityInterface( fields=[ 'subject_id', 'in_fmap_mag', 'in_fmap_pha', 'in_t1w_brain', 'in_t2w', 'fs_subjects_dir','te_incr', 'echospacing','enc_dir' ]), name='inputnode' )
+    outputnode = pe.Node(niu.IdentityInterface(fields=['out_file', 'out_vsm', 'out_mask', 'out_tpms' ]), name='outputnode' )
+    
+    # Setup internal workflows
+    prepare = smri_preparation()
+    distort = distortion_workflow()
+
+
+    workflow.connect( [
+                         ( inputnode,  prepare, [ ('subject_id','inputnode.subject_id'),('in_fmap_mag','inputnode.in_fmap_mag'),
+                                                  ('in_fmap_pha','inputnode.in_fmap_pha'),('in_t1w_brain','inputnode.in_t1w_brain'),
+                                                  ('in_t2w','inputnode.in_t2w'),('fs_subjects_dir','inputnode.fs_subjects_dir') ])
+                        ,( inputnode,  distort, [ ('te_incr','inputnode.te_incr'),('echospacing','inputnode.echospacing'),('enc_dir','inputnode.enc_dir') ])
+                        ,( prepare,    distort, [ ('outputnode.out_smri','inputnode.in_file'),('outputnode.out_fmap_pha','inputnode.in_pha'),
+                                                  ('outputnode.out_fmap_mag','inputnode.in_mag'),('outputnode.out_mask','inputnode.in_mask'),
+                                                  ('outputnode.out_tpms','inputnode.in_tpms') ])
+                        ,( distort, outputnode, [ ('outputnode.out_file','out_file'),('outputnode.out_vsm','out_vsm'),
+                                                  ('outputnode.out_mask','out_mask'),('outputnode.out_tpms','out_tpms') ])
+                    ])
+
+    return workflow 
+
 
 def smri_preparation( name="sMRI_prepare" ):
     workflow = pe.Workflow(name=name)
