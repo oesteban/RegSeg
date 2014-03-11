2885b8c4e907d81b27e03eab59d75fcbe501cefc
Oscar Esteban <code@oscaresteban.es>
2014-03-11 12:46:18 +0100

    Closed forever orientation issues.
    Now it is clear: ITK uses LPS convention, and reverses the
    matrix when loading volumes. Conversely, VTK files are assumed
    to be in world coordinates directly.
    
    Then, Freesurfer has RAS convention. This means that a freesurfer file
    with RAS orientation will be an image with LPS orientation matrix in
    ITK.
    
    Additionally, mris_convert does not add the geometrical information
    to the vertices. Thus, the generated vtk files are in the so-called
    tkRAS coordinate space (the one of freeview and such tools).
    
    A new function named fixvtk has been added to transform all the
    vertices to a target scannerRAS coordinates (world space).
    
    I tested it on one case, and regions generated by ACWEReg are consistent
    with the resampled volume that is internally used in energy computations.
    
    This enables that we generate the datasets of #118

--- Scripts/pyacwereg/utils/freesurfer.py
+++ Scripts/pyacwereg/utils/freesurfer.py
@@ -11,32 +11,55 @@ from nipype.interfaces.io import IOBase
 
 
 class FSFilesInputSpect( BaseInterfaceInputSpec ):
-	fs_subjects_dir = Directory( os.environ['SUBJECTS_DIR'], exists=True, usedefault=True, 
-		                         desc='Freesurfer subjects dir' )
-	subject_id = traits.String( mandatory=True, desc='Subject in Freesurfer system')
+    fs_subjects_dir = Directory( os.environ['SUBJECTS_DIR'], exists=True, usedefault=True, 
+                                 desc='Freesurfer subjects dir' )
+    subject_id = traits.String( mandatory=True, desc='Subject in Freesurfer system')
 
 class FSFilesOutputSpect( TraitedSpec ):
-	aseg = File(exists=True, desc='Path to aseg.mgz file' )
-	rawavg = File(exists=True, desc='Path to rawavg.mgz file' )
-	orig = File(exists=True, desc='Path to orig.mgz file' )
-	norm = File(exists=True, desc='Path to norm.mgz file' )
+    aseg = File(exists=True, desc='Path to aseg.mgz file' )
+    rawavg = File(exists=True, desc='Path to rawavg.mgz file' )
+    orig = File(exists=True, desc='Path to orig.mgz file' )
+    norm = File(exists=True, desc='Path to norm.mgz file' )
 
 class FSFiles( IOBase ):
-	input_spec = FSFilesInputSpect
-	output_spec = FSFilesOutputSpect
+    """ Dumb datasource to get Freesurfer's files.
 
-	def _list_outputs( self ):
-		fs_dir = op.join( self.inputs.fs_subjects_dir, self.inputs.subject_id, 'mri' )
-		outputs = self._outputs().get()
-		outputs['aseg'] = op.join( fs_dir, 'aseg.mgz' )
-		outputs['rawavg'] = op.join( fs_dir, 'rawavg.mgz' )
-		outputs['orig'] = op.join( fs_dir, 'orig.mgz' )
-		outputs['norm'] = op.join( fs_dir, 'norm.mgz' )
+    Returns the path of different files of interest within the Freesurfer folder of a
+    certain subject.
 
-		return outputs
+    Examples
+    --------
+
+    >>> from pyacwereg.utils.freesurfer import FSFiles
+    >>> fs_files = FSFiles()
+    >>> fs_files.inputs.fs_subjects_dir = './FREESURFER'
+    >>> fs_files.inputs.subject_id = 'S400'
+    >>> fs_files.run()
+    """
+    input_spec = FSFilesInputSpect
+    output_spec = FSFilesOutputSpect
+
+    def _list_outputs( self ):
+        fs_dir = op.join( self.inputs.fs_subjects_dir, self.inputs.subject_id, 'mri' )
+        outputs = self._outputs().get()
+        outputs['aseg'] = op.join( fs_dir, 'aseg.mgz' )
+        outputs['rawavg'] = op.join( fs_dir, 'rawavg.mgz' )
+        outputs['orig'] = op.join( fs_dir, 'orig.mgz' )
+        outputs['norm'] = op.join( fs_dir, 'norm.mgz' )
+
+        return outputs
 
 
 def gen_fs_transform( in_file, fs_subjects_dir, subject_id, out_file=None ):
+    """ Get transform matrix between orig (*tkRAS*) and native (*scannerRAS*)
+    coordinates in Freesurfer.
+
+    Keyword arguments:
+    in_file -- image in scanner coordinates
+    fs_subjects_dir -- Freesurfer's SUBJECTS_DIR environment variable
+    subject_id -- Subject identifier in Freesufer's folder tree
+    out_file (optional) -- Filename for the transform matrix
+    """
     import os.path as op
     import subprocess as sp
 
@@ -49,6 +72,16 @@ def gen_fs_transform( in_file, fs_subjects_dir, subject_id, out_file=None ):
     return out_file
 
 def transform_surface( in_surf, in_reg, in_target, fs_subjects_dir, subject_id, out_file=None ):
+    """ Apply a transform matrix to in_surf
+
+    Keyword arguments:
+    in_surf -- surface to be *resampled*
+    in_reg -- Freesurfer's transform file
+    in_target -- target image to get coordinates from
+    fs_subjects_dir -- Freesurfer's SUBJECTS_DIR environment variable
+    subject_id -- Subject identifier in Freesufer's folder tree
+    out_file (optional) -- Filename for the resampled surface
+    """
     import os
     import os.path as op
     import subprocess as sp
@@ -72,6 +105,7 @@ def transform_surface( in_surf, in_reg, in_target, fs_subjects_dir, subject_id, 
     return out_file
 
 def MRIPreTess( in_file, in_norm, label=1, out_file="" ):
+    """ Wraps mri_pretess command to generate a surface """
     import subprocess as sp
     import os
     import os.path as op
@@ -90,6 +124,7 @@ def MRIPreTess( in_file, in_norm, label=1, out_file="" ):
 
 
 def merge_labels( in_file, labels ):
+    """ Binarizes a segmentation (`in_file`) by picking up labels only """
     import nibabel as nb
     import numpy as np
     import os.path as op
@@ -116,9 +151,13 @@ def merge_labels( in_file, labels ):
     return out_file
 
 def fixvtk( in_file, in_ref, out_file=None ):
+    """ Transforms an vtk file from Freesurfer's *tkRAS* coordinates
+    to a target image in *scannerRAS* coordinates.
+    """
     import nibabel as nb
     import numpy as np
     import os.path as op
+    import subprocess as sp
 
     if out_file is None:
         fname,ext = op.splitext( op.basename(in_file) )
@@ -128,16 +167,12 @@ def fixvtk( in_file, in_ref, out_file=None ):
         out_file= op.abspath( "%s_fixed.vtk" % fname )
 
     ref = nb.load( in_ref )
-    orig = np.array( ref.get_shape() ) * 0.5
-    ac = ref.get_affine()[0:3,0:3]
-    
-    rotM = np.zeros( shape=ac.shape )
-    rotM[0,0] = -1.0
-    rotM[1,2] = 1.0
-    rotM[2,1] = -1.0
-    
-    R = np.dot( rotM, ac )
-        
+    cmd_info = "mri_info --tkr2scanner %s" % in_ref
+    proc = sp.Popen( cmd_info, stdout=sp.PIPE, shell=True )
+    data = bytearray( proc.stdout.read() )
+    mstring =  np.fromstring(data.decode("utf-8"), sep='\n' )
+    matrix = np.reshape(mstring, (4,-1) )
+
     with open( in_file, 'r' ) as f:
         with open( out_file, 'w+' ) as w:
             npoints = 0
@@ -149,9 +184,10 @@ def fixvtk( in_file, in_ref, out_file=None ):
                     npoints = int( s[1] )
                     fmt = np.dtype( s[2] )
                 elif( i>4 and pointid<npoints):
-                    vert = np.array( [float(x) for x in l.split()] )
-                    vert = np.dot( vert, R ) + orig
-                    l = '%.9f  %.9f  %.9f\n' % tuple(vert)
+                    vert = [float(x) for x in l.split()]
+                    vert.append( 1.0 )
+                    newvert = np.dot( matrix, vert )
+                    l = '%.9f  %.9f  %.9f\n' % tuple(newvert[0:3])
                 
                 w.write( l )
                 pointid = pointid + 1
