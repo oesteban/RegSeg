88f927854d7c995fd0abdc7707c6d6868b0dc9e4
Oscar Esteban <code@oscaresteban.es>
2014-03-20 12:41:38 +0100

    Fix workflow

--- Scripts/pyacwereg/workflows/distortion.py
+++ Scripts/pyacwereg/workflows/distortion.py
@@ -5,8 +5,8 @@
 #
 # @Author: oesteban
 # @Date:   2014-03-11 15:28:16
-# @Last Modified by:   Oscar Esteban
-# @Last Modified time: 2014-03-13 10:21:50
+# @Last Modified by:   oesteban
+# @Last Modified time: 2014-03-20 12:41:31
 
 import os
 import os.path as op
@@ -25,16 +25,16 @@ from pyacwereg.nipype.interfaces import RandomBSplineDeformation
 def isbi_workflow( name='ISBI2014' ):
     workflow = pe.Workflow(name=name)
     # Setup i/o
-    inputnode = pe.Node( niu.IdentityInterface(fields=['subject_id','in_fmap_mag','in_fmap_pha','in_t1w_brain', 'in_t2w', 'fs_subjects_dir','te_incr', 'echospacing','enc_dir' ]), name='inputnode' )
+    inputnode = pe.Node( niu.IdentityInterface(fields=['subject_id','in_fmap_mag','in_fmap_pha','in_t1w', 'in_t2w', 'fs_subjects_dir','te_incr', 'echospacing','enc_dir' ]), name='inputnode' )
     outputnode = pe.Node(niu.IdentityInterface(fields=['out_file', 'out_vsm', 'out_mask', 'out_tpms' ]), name='outputnode' )
-    
+
     # Set-up internal workflows
     prepare = fieldmap_preparation()
     distort = epi_deform()
 
     workflow.connect([
                          ( inputnode,  prepare, [ ('subject_id','inputnode.subject_id'),('in_fmap_mag','inputnode.in_fmap_mag'),
-                                                  ('in_fmap_pha','inputnode.in_fmap_pha'),('in_t1w_brain','inputnode.in_t1w_brain'),
+                                                  ('in_fmap_pha','inputnode.in_fmap_pha'),('in_t1w','inputnode.in_t1w'),
                                                   ('in_t2w','inputnode.in_t2w'),('fs_subjects_dir','inputnode.fs_subjects_dir') ])
                         ,( inputnode,  distort, [ ('te_incr','inputnode.te_incr'),('echospacing','inputnode.echospacing'),('enc_dir','inputnode.enc_dir') ])
                         ,( prepare,    distort, [ ('outputnode.out_smri','inputnode.in_file'),('outputnode.out_fmap_pha','inputnode.in_pha'),
@@ -44,7 +44,7 @@ def isbi_workflow( name='ISBI2014' ):
                                                   ('outputnode.out_mask','out_mask'),('outputnode.out_tpms','out_tpms') ])
                     ])
 
-    return workflow 
+    return workflow
 
 def bspline_deform( name='BSplineDistortion' ):
     """ A nipype workflow to produce bspline-based deformation fields """
