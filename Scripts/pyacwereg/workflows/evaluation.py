42e84f5117338b03ac432bc0ba6680b928a97dad
Oscar Esteban <code@oscaresteban.es>
2014-03-28 13:16:58 +0100

    Fixing workflow

--- Scripts/pyacwereg/workflows/evaluation.py
+++ Scripts/pyacwereg/workflows/evaluation.py
@@ -5,8 +5,8 @@
 #
 # @Author: Oscar Esteban - code@oscaresteban.es
 # @Date:   2014-03-12 16:59:14
-# @Last Modified by:   Oscar Esteban
-# @Last Modified time: 2014-03-13 10:21:44
+# @Last Modified by:   oesteban
+# @Last Modified time: 2014-03-28 13:06:28
 
 import os
 import os.path as op
@@ -23,7 +23,8 @@ def bspline( name='BSplineEvaluation' ):
     """
     wf = pe.Workflow( name=name )
     inputnode = pe.Node( niu.IdentityInterface( fields=[ 'subject_id', 'data_dir','grid_size' ] ), name='inputnode' )
-    outputnode = pe.Node(niu.IdentityInterface(fields=['out_file', 'out_field', 'out_coeff' ]), name='outputnode' )
+    outputnode = pe.Node(niu.IdentityInterface(fields=['out_file', 'out_tpms', 'out_surfs',
+                                               'out_field', 'out_coeff' ]), name='outputnode' )
 
     prep = prepare_smri()
     dist = bspline_deform()
@@ -31,7 +32,9 @@ def bspline( name='BSplineEvaluation' ):
     wf.connect([
              ( inputnode, prep, [ ('subject_id','inputnode.subject_id'),('data_dir','inputnode.data_dir') ])
             ,( inputnode, dist, [ ('grid_size', 'inputnode.grid_size')])
-            ,( prep,      dist, [ ('outputnode.out_smri_brain','inputnode.in_file')])
+            ,( prep,      dist, [ ('outputnode.out_smri_brain','inputnode.in_file'),
+                                  ('outputnode.out_surfs', 'inputnode.in_source')
+                                  ('outputnode.out_tpms', 'inputnode.in_tpms')])
             ,( dist, outputnode, [ ('outputnode.out_file','out_file'),('outputnode.out_field','out_field'),('outputnode.out_coeff','out_coeff')])
         ])
 
