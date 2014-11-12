c0c310c0b806f33c4b588361ec19f984674842b8
Oscar Esteban <code@oscaresteban.es>
2014-11-12 19:20:45 +0100

    generate cerebellar GM. close #173

--- Scripts/tools/run_evaluations.py
+++ Scripts/tools/run_evaluations.py
@@ -5,8 +5,8 @@
 #
 # @Author: oesteban - code@oscaresteban.es
 # @Date:   2014-04-04 19:39:38
-# @Last Modified by:   Oscar Esteban
-# @Last Modified time: 2014-11-06 20:16:01
+# @Last Modified by:   oesteban
+# @Last Modified time: 2014-11-12 19:20:12
 
 __author__ = "Oscar Esteban"
 __copyright__ = "Copyright 2013, Biomedical Image Technologies (BIT), \
@@ -137,16 +137,16 @@ def hcp_workflow(name='HCP_TMI2015', settings={}):
 
     regseg = regseg_wf()
     regseg.inputs.inputnode.iterations = [150, 100, 100]
-    regseg.inputs.inputnode.descript_update = [26, 16, 11]
-    regseg.inputs.inputnode.step_size = [200.0, 100.0, 10.0]
+    regseg.inputs.inputnode.descript_update = [21, 16, 11]
+    regseg.inputs.inputnode.step_size = [0.05, 0.1, 0.1]
     regseg.inputs.inputnode.alpha = [0.0, 0.0, 0.0]
     regseg.inputs.inputnode.beta = [0.0, 0.0, 0.0]
     regseg.inputs.inputnode.convergence_energy = [True, True, True]
-    regseg.inputs.inputnode.convergence_window = [25, 15, 10]
-    regseg.inputs.inputnode.f_smooth = [2.4, 2.0, None]
+    regseg.inputs.inputnode.convergence_window = [20, 15, 10]
+    regseg.inputs.inputnode.f_smooth = [2.4, 1.2, None]
     regseg.inputs.inputnode.images_verbosity = 3
     regseg.inputs.inputnode.scales = [(0.0, 1.0, 0.0)] * 3
-    regseg.inputs.inputnode.grid_size = [(6, 4, 6), 6, 8]
+    regseg.inputs.inputnode.grid_size = [4, 4, (8, 6, 8)]
 
     wf.connect([
         (st1,   dti,    [('out_dis_set.dwi', 'inputnode.in_dwi'),
