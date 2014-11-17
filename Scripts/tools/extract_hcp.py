77d71e5bb5df85c7f1279df150831ca20e6deb34
oesteban <code@oscaresteban.es>
2014-11-17 17:01:27 +0100

    fix extraction script

--- Scripts/tools/extract_hcp.py
+++ Scripts/tools/extract_hcp.py
@@ -15,12 +15,16 @@ filesmap = [('T1w/aparc+aseg.nii.gz', 'aparc+aseg.nii.gz'),
             ('T1w/Diffusion/data.nii.gz', 'dwi.nii.gz'),
             ('T1w/Diffusion/nodif_brain_mask.nii.gz', 'dwi_brainmask.nii.gz'),
             ('T1w/T1w_acpc_dc_restore.nii.gz', 'T1w_acpc_dc_restore.nii.gz'),
-            ('T1w/T2w_acpc_dc_restore.nii.gz', 'T2w_acpc_dc_restore.nii.gz')]
+            ('T1w/T2w_acpc_dc_restore.nii.gz', 'T2w_acpc_dc_restore.nii.gz'),
+            ('T1w/T1w_acpc_dc_restore_brain.nii.gz',
+             'T1w_acpc_dc_restore_brain.nii.gz'),
+            ('T1w/T2w_acpc_dc_restore_brain.nii.gz',
+             'T2w_acpc_dc_restore_brain.nii.gz')]
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     from argparse import RawTextHelpFormatter
-    from tmpfile import mkdtemp
+    from tempfile import mkdtemp
     import subprocess as sp
     import shutil as sh
 
@@ -28,7 +32,7 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='Extract HCP data for TMI2015 paper',
                             formatter_class=RawTextHelpFormatter)
     g_input = parser.add_argument_group('Input')
-    g_input.add_argument('-i', '--input', action='store')
+    g_input.add_argument('-i', '--input', nargs='+', action='store')
     g_input.add_argument('-d', '--dest', action='store', default='.')
 
     opts = parser.parse_args()
@@ -41,26 +45,38 @@ if __name__ == '__main__':
         if not op.exists(iname):
             raise IOError('Input package not found')
 
+        iname = op.abspath(iname)
+
         bname = op.basename(iname)
+        dname = op.dirname(iname)
         caseid = bname.split('_', 1)[0]
 
-        stname = op.dirname(iname) + bname.split('Diffusion', 1)[0]
+        stname = op.join(dname, bname.split('Diffusion', 1)[0])
         stname += 'Structural_preproc.zip'
 
         tmpdir = mkdtemp()
         os.chdir(tmpdir)
         try:
-            sp.check_output(['unzip', op.abspath(iname)])
-            sp.check_output(['unzip', op.abspath(stname)])
-
+            sp.check_output(['unzip', iname])
+            sp.check_output(['unzip', stname])
             dstfld = op.join(dest, 'HCP%s' % caseid)
-            os.mkdir(dstfld)
 
-            for fo, fd in filesmap:
-                sh.copyfile(op.abspath(op.join(tmpdir, fo)),
-                            op.abspath(dstfld, fd))
-        except:
+            try:
+                sh.rmtree(dstfld)
+            except:
+                pass
+
+            os.mkdir(dstfld)
+            
+            for f in filesmap:
+                forig = op.abspath(op.join(tmpdir, caseid, f[0]))
+                fdest = op.abspath(op.join(dstfld, f[1]))
+                print 'Copying: %s -> %s' % (forig, fdest)
+                sh.copy(forig, fdest)
+        except Exception as e:
             print 'Extracting %s failed' % iname
+            print e
+            print os.listdir(tmpdir)
             pass
 
         os.chdir(curdir)
