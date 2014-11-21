b2800eb6404d8971d68f0cb4456f84499795202a
Oscar Esteban <code@oscaresteban.es>
2014-11-21 14:30:27 +0100

    just to get changes registered, rolling back #174

--- Code/Applications/slice_contours.h
+++ Code/Applications/slice_contours.h
@@ -43,6 +43,17 @@
 #ifndef SLICE_CONTOURS_H_
 #define SLICE_CONTOURS_H_
 
+// See: http://www.vtk.org/Wiki/VTK/VTK_6_Migration/Factories_now_require_defines
+// See: https://gist.github.com/certik/5687727
+#include <vtkVersion.h>
+
+#if VTK_MAJOR_VERSION <= 5
+	#include <vtkGraphicsFactory.h>
+	#include <vtkImagingFactory.h>
+#else
+	#include <vtkAutoInit.h>
+#endif
+
 
 #include <boost/program_options.hpp>
 #include <boost/lexical_cast.hpp>
@@ -51,7 +62,6 @@
 
 #include <vector>
 
-#include <vtkVersion.h>
 #include <vtkSmartPointer.h>
 #include <vtkActor.h>
 #include <vtkProperty.h>
@@ -80,30 +90,26 @@
 #include <vtkCornerAnnotation.h>
 #include <vtkTextActor.h>
 #include <vtkTextProperty.h>
+#include <vtkMatrix4x4.h>
+#include <vtkLookupTable.h>
+#include <vtkImageMapToColors.h>
 
 #include <itkImage.h>
 #include <itkImageFileReader.h>
 #include <itkRescaleIntensityImageFilter.h>
 #include <itkImageToVTKImageFilter.h>
 
+#include <vtkImageMagnify.h>
+#include <vtkImageViewer2.h>
+#include <vtkImageReslice.h>
 
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkInteractorStyleImage.h>
 #include <vtkRenderer.h>
 #include <vtkWindowToImageFilter.h>
+#include <vtkImageMapToWindowLevelColors.h>
 
-#if VTK_MAJOR_VERSION == 6
-// These 2 lines are needed due to:
-// http://www.vtk.org/Wiki/VTK/VTK_6_Migration/Factories_now_require_defines
-// Taken from: https://gist.github.com/certik/5687727
-#include <vtkAutoInit.h>
-#define vtkRenderingCore_AUTOINIT 4(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingFreeTypeOpenGL,vtkRenderingOpenGL)
-#define vtkRenderingVolume_AUTOINIT 1(vtkRenderingVolumeOpenGL)
-#else
-#include <vtkGraphicsFactory.h>
-#include <vtkImagingFactory.h>
-#endif
 
 namespace bpo = boost::program_options;
 namespace bfs = boost::filesystem;
@@ -116,6 +122,25 @@ typedef itk::ImageFileReader<ImageType>                      ReaderType;
 typedef typename ReaderType::Pointer                         ReaderPointer;
 typedef itk::ImageToVTKImageFilter<ImageType>                ConnectorType;
 typedef itk::RescaleIntensityImageFilter< ImageType, ImageType > RescaleFilterType;
+
+// Matrices for axial, coronal, sagittal, oblique view orientations
+static const double axialElements[16] = {
+         1, 0, 0, 0,
+         0, 1, 0, 0,
+         0, 0, 1, 0,
+         0, 0, 0, 1 };
+
+static const double coronalElements[16] = {
+         1, 0, 0, 0,
+         0, 0, 1, 0,
+         0,-1, 0, 0,
+         0, 0, 0, 1 };
+
+static const double sagittalElements[16] = {
+         0, 0,-1, 0,
+         1, 0, 0, 0,
+         0,-1, 0, 0,
+         0, 0, 0, 1 };
 
 int main(int argc, char *argv[]);
 
