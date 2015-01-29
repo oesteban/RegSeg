ee597b398dbd6a39f7e3c0382822aee3ae52c78c
Oscar Esteban <code@oscaresteban.es>
2015-01-29 14:22:07 +0100

    extract fieldmaps. close #208

--- Scripts/tools/extract_hcp.py
+++ Scripts/tools/extract_hcp.py
@@ -3,25 +3,48 @@
 # @Author: oesteban
 # @Date:   2014-11-17 15:32:49
 # @Last Modified by:   oesteban
-# @Last Modified time: 2014-11-17 23:49:32
+# @Last Modified time: 2015-01-29 14:15:45
 
-import os
-import os.path as op
+filesmap = {}
 
-filesmap = [('T1w/aparc+aseg.nii.gz', 'aparc+aseg.nii.gz'),
-            ('T1w/brainmask_fs.nii.gz', 'brainmask_fs.nii.gz'),
-            ('T1w/Diffusion/bvals', 'bvals'),
-            ('T1w/Diffusion/bvecs', 'bvecs'),
-            ('T1w/Diffusion/data.nii.gz', 'dwi.nii.gz'),
-            ('T1w/Diffusion/nodif_brain_mask.nii.gz', 'dwi_brainmask.nii.gz'),
-            ('T1w/T1w_acpc_dc_restore.nii.gz', 'T1w_acpc_dc_restore.nii.gz'),
-            ('T1w/T2w_acpc_dc_restore.nii.gz', 'T2w_acpc_dc_restore.nii.gz'),
-            ('T1w/T1w_acpc_dc_restore_brain.nii.gz',
-             'T1w_acpc_dc_restore_brain.nii.gz'),
-            ('T1w/T2w_acpc_dc_restore_brain.nii.gz',
-             'T2w_acpc_dc_restore_brain.nii.gz')]
+filesmap['struct_preproc'] = [
+    ('T1w/aparc+aseg.nii.gz', 'aparc+aseg.nii.gz'),
+    ('T1w/brainmask_fs.nii.gz', 'brainmask_fs.nii.gz'),
+    ('T1w/T1w_acpc_dc_restore.nii.gz', 'T1w_acpc_dc_restore.nii.gz'),
+    ('T1w/T2w_acpc_dc_restore.nii.gz', 'T2w_acpc_dc_restore.nii.gz'),
+    ('T1w/T1w_acpc_dc_restore_brain.nii.gz',
+     'T1w_acpc_dc_restore_brain.nii.gz'),
+    ('T1w/T2w_acpc_dc_restore_brain.nii.gz',
+     'T2w_acpc_dc_restore_brain.nii.gz')]
+
+filesmap['diff_preproc'] = [
+    ('T1w/Diffusion/bvals', 'bvals'),
+    ('T1w/Diffusion/bvecs', 'bvecs'),
+    ('T1w/Diffusion/data.nii.gz', 'dwi.nii.gz'),
+    ('T1w/Diffusion/nodif_brain_mask.nii.gz', 'dwi_brainmask.nii.gz')]
+
+
+filesmap['struct_unproc'] = [
+    ('unprocessed/{tesla}/T1w_MPR1/{caseid}_{tesla}_FieldMap_Magnitude.nii.gz',
+     'FM_mag.nii.gz'),
+    ('unprocessed/{tesla}/T1w_MPR1/{caseid}_{tesla}_FieldMap_Phase.nii.gz',
+     'FM_pha.nii.gz')]
+
+defparms = {'enc_dir': 'y-',
+            'echospacing': 7.800117313764398e-4,
+            'te_incr': 2.46e-3,
+            'epi_factor': 128,
+            'epi_lines': 57,
+            'epi_acc': 1,
+            'field_strength': 3.0,
+            'field_axis': 'z'
+            }
 
 if __name__ == '__main__':
+    import os
+    import errno
+    import os.path as op
+    import json
     from argparse import ArgumentParser
     from argparse import RawTextHelpFormatter
     from tempfile import mkdtemp
@@ -34,6 +57,7 @@ if __name__ == '__main__':
     g_input = parser.add_argument_group('Input')
     g_input.add_argument('-i', '--input', nargs='+', action='store')
     g_input.add_argument('-d', '--dest', action='store', default='.')
+    g_input.add_argument('-t', '--tmpdir', action='store', default='/tmp/')
 
     opts = parser.parse_args()
     if opts.dest == '.':
@@ -49,32 +73,65 @@ if __name__ == '__main__':
 
         bname = op.basename(iname)
         dname = op.dirname(iname)
-        caseid = bname.split('_', 1)[0]
+        bnameparts = bname.split('_')
+        caseid = bnameparts[0]
+        tesla = bnameparts[1]
 
         stname = op.join(dname, bname.split('Diffusion', 1)[0])
         stname += 'Structural_preproc.zip'
 
-        tmpdir = mkdtemp()
+        stname2 = op.join(dname, bname.split('Diffusion', 1)[0])
+        stname2 += 'Structural_unproc.zip'
+
+        tmpdir = mkdtemp(prefix=opts.tmpdir)
+        zipnames = (iname, stname, stname2)
         os.chdir(tmpdir)
         try:
-            sp.check_output(['unzip', iname])
-            sp.check_output(['unzip', stname])
             dstfld = op.join(dest, 'HCP%s' % caseid)
-
             try:
-                sh.rmtree(dstfld)
-            except:
-                pass
+                os.makedirs(dstfld)
+            except OSError as exc:  # Python >2.5
+                if exc.errno == errno.EEXIST and op.isdir(dstfld):
+                    pass
+                else:
+                    raise
 
-            os.mkdir(dstfld)
+            for n, v in zip(zipnames, ['diff_preproc',
+                                       'struct_preproc', 'struct_unproc']):
+                print 'Extracting HCP%s_%s to %s' % (caseid, tesla, tmpdir)
+                ofiles = [f[0].format(tesla=tesla, caseid=caseid)
+                          for f in filesmap[v]]
+                dfiles = [f[1].format(tesla=tesla, caseid=caseid)
+                          for f in filesmap[v]]
 
-            for f in filesmap:
-                forig = op.abspath(op.join(tmpdir, caseid, f[0]))
-                fdest = op.abspath(op.join(dstfld, f[1]))
-                print 'Copying: %s -> %s' % (forig, fdest)
-                sh.copy(forig, fdest)
+                rewrite = [fid for fid, f in enumerate(dfiles)
+                           if op.exists(op.join(dstfld, f))]
+
+                rfiles = []
+                for idx in reversed(rewrite):
+                    rfiles.append(ofiles.pop(idx))
+                    dfiles.pop(idx)
+
+                if len(ofiles) > 0:
+                    flist = [op.join(caseid, f.format(tesla=tesla, caseid=caseid))
+                             for f in ofiles]
+
+                    sp.check_output(['unzip', n] + flist)
+                    for of, df in zip(flist, dfiles):
+                        forig = op.abspath(of)
+                        fdest = op.abspath(op.join(dstfld, df))
+                        print 'Copying: %s -> %s' % (forig, fdest)
+                        sh.copy(forig, fdest)
+
+                if len(rfiles) > 0:
+                    print 'Skipped %s' % ' '.join(rfiles)
+
+            if not op.exists(op.join(dstfld, 'parameters.txt')):
+                with open(op.join(dstfld, 'parameters.txt'), 'w') as pf:
+                    json.dump(defparms, pf)
+
         except Exception as e:
-            print 'Extracting %s failed' % iname
+            print 'Unpacking %s failed' % iname
             print e
             print os.listdir(tmpdir)
             pass
