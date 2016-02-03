52276939b3601e4e4bbd9097e3ba296df9c03689
Oscar Esteban <code@oscaresteban.es>
2016-02-03 09:34:14 -0800

    revised paper

--- Publications/2015-NeuroImage/visual_report.sh
+++ Publications/2015-NeuroImage/visual_report.sh
@@ -4,7 +4,8 @@ rm -f suppl-realdata.tex
 
 for i in supplemental/HCP*.pdf; do
     fname=$(basename $i)
-    fname=${fname::-4}
+    fname=${fname:0:9}
+    echo $fname
     echo "\\begin{figure}[!ht]" >> suppl-realdata.tex
     echo "  \\centering" >> suppl-realdata.tex
     echo "  \\textbf{$fname}" >> suppl-realdata.tex
